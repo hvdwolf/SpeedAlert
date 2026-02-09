6d6b638cc7860ea273c699f8516ff3d5377f30d9
hvdwolf <hvdwolf@gmail.com>
2026-02-09 14:40:47 +0100

    Now really set speed limit retrieval to 4 seconds

--- app/src/main/java/xyz/hvdw/speedalert/DrivingService.kt
+++ app/src/main/java/xyz/hvdw/speedalert/DrivingService.kt
@@ -224,7 +224,7 @@ class DrivingService : Service() {
         val lon = loc.longitude
         val now = System.currentTimeMillis()
 
-        val result = if (now - lastLimitFetchTime > 5_000) {
+        val result = if (now - lastLimitFetchTime > 4_000) {
             lastLimitFetchTime = now
 
             log("Service: calling repo for $lat,$lon")
