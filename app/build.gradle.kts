f5e8d6ad6b6113d777ddb1cbb793fcd50e7b285c
hvdwolf <hvdwolf@gmail.com>
2026-02-14 10:02:12 +0100

    forgot to push the app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "xyz.hvdw.speedalert"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         // Only include the ABIs you want in the final APK
         ndk {
